7e62d4ce0ee64b7443417bdfa916acfb1157bc82
Martin Brown <35014201+martingit9@users.noreply.github.com>
2020-06-08 20:33:11 +0100

    Google fonts added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
 function SelectNumbers({ }) {
   const [userNumbers, setUserNumbers] = useState([]);
   const [winningMessage, setWinningMessage] = useState('');
