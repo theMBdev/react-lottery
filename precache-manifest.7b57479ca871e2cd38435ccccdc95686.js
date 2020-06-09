90c6670b63cd1f1d4f5fd5dac3f77cbf9b851d39
Martin Brown <35014201+martingit9@users.noreply.github.com>
2020-06-09 16:50:33 +0100

    Updates

---
+++ precache-manifest.7b57479ca871e2cd38435ccccdc95686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0fe566a99802ce5c36b011f74e536d",
+    "url": "/theMBdev/react-lottery/index.html"
+  },
+  {
+    "revision": "20a5612a71a58db20d4e",
+    "url": "/theMBdev/react-lottery/static/css/main.a74b4391.chunk.css"
+  },
+  {
+    "revision": "677cfc82aca3abf4e8f3",
+    "url": "/theMBdev/react-lottery/static/js/2.7c7335f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theMBdev/react-lottery/static/js/2.7c7335f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a5612a71a58db20d4e",
+    "url": "/theMBdev/react-lottery/static/js/main.25c4df3b.chunk.js"
+  },
+  {
+    "revision": "ed4d74b5c3b58b81939c",
+    "url": "/theMBdev/react-lottery/static/js/runtime-main.780031f6.js"
+  }
+]);
